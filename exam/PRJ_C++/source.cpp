66f7ac3b524cd42c54822f99a6f0c63bdcd03e97
Gabriele <gabriele@gabri.lan>
2021-03-31 20:13:20 +0200

    cp sem, find, insert, clear

--- exam/PRJ_C++/source.cpp
+++ exam/PRJ_C++/source.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include <utility>
 #include <memory>
-#include "iterator_bst.cpp"
+//#include "iterator_bst.cpp"
+//#include "node.cpp"
 
 
 
@@ -9,22 +10,34 @@
 template <typename t_k, typename t_v, typename OP = std::less<t_k> >
 class bst{
 
-    //define struct node
+    // NODE STRUCT
     struct node{
         std::pair<const t_k, t_v> _pair;
         std::unique_ptr<node> left;
         std::unique_ptr<node> right;
+        std::unique_ptr<node> father;
 
-        node(): _pair{}, left{nullptr}, right{nullptr} {}
-        node(const t_k& key, const t_v& value) noexcept : _pair{key, value}, left{nullptr}, right{nullptr} {}
-        node(t_k&& key, t_v&& value) noexcept : _pair{std::move(key),std::move(value)}, left{nullptr}, right{nullptr}  {}
-        node(const std::pair<t_k,t_v>& pair): _pair{pair}, left{nullptr}, right{nullptr}  {}
-        node(std::pair<t_k,t_v>&& pair): _pair{std::move(pair)}, left{nullptr}, right{nullptr}  {}
+        node() noexcept = default;
+        node(const t_k& key, const t_v& value) noexcept : _pair{key, value}, left{nullptr}, right{nullptr}, father{nullptr} {}
+        node(t_k&& key, t_v&& value) noexcept : _pair{std::move(key),std::move(value)}, left{nullptr}, right{nullptr}, father{nullptr}  {}
+        node(const std::pair<t_k,t_v>& pair): _pair{pair}, left{nullptr}, right{nullptr}, father{nullptr}  {}
+        node(std::pair<t_k,t_v>&& pair): _pair{std::move(pair)}, left{nullptr}, right{nullptr}, father{nullptr}  {}
         ~node() noexcept = default;
-
-        // should I define cp sem.?
-
-
+        // define a ctor to be used in the cp ctor of bst
+        explicit node(const std::unique_ptr<node>& x): _pair{x->_pair}{
+            // take care of left and right children:
+            // on the right:
+            if(x->right){
+                right.reset(new node{x->right});
+            }
+            else{right.reset();}
+            // on the left:
+            if(x->left){
+                left.reset(new node{x->left});
+            }
+            else{left.reset();}
+            // the idea is to call recursively the ctor (of node) on every side untill we meet a nullptr
+        }
 
         friend
         std::ostream& operator<<(std::ostream& os, const node& x){
@@ -32,13 +45,113 @@ class bst{
             return os;
         }
     };
+    // ####################### END NODE STRUCT DEF
 
-    //the bst must have a node and a ptr to the root
+    // CLASS ITERATOR
+    template <typename O>
+    class _iterator{
+        using node = typename bst<t_k,t_v,OP>::node;
+        node* current;      // a raw ptr to node is enough as class member
+
+    public:
+        using value_type = O;
+        using difference_type = std::ptrdiff_t;
+        using iterator_category = std::forward_iterator_tag;
+        using reference = value_type&;
+        using pointer = value_type*;
+    
+        // the default ctor should be initializated
+        // such that it points to the left most node in the bst
+        _iterator() noexcept: current{begin()} {} 
+        // I don't want to define an implicit conversion
+        // from node* to Iterator
+        explicit _iterator(node* x) noexcept: current{x} {}      // evaluate to put a const infront of node* x
+        explicit _iterator(node&& x) noexcept: current{std::move(x)} {}
+        // default destructor is ok
+        ~_iterator() noexcept = default;
+
+        _iterator& operator++() {
+        // here I must think how to move
+        // from the current node to the next.
+        // what does next means in this case?
+
+        // in general the lesson example for L.L. was:
+        // current = curent->next.get()
+        // return *this
+        return *this;
+        }
+
+        pointer operator->() noexcept{return &**this;}
+        reference operator*() const noexcept{return current->_pair.first;} // we are interested in the key 
+                                                                       // dereferencing iterator must 
+                                                                       // give me the key
+                                                                    
+        friend
+        bool operator==(const _iterator& a, const _iterator& b) noexcept {
+            return a.current == b.current;   // two nodes are equal when they point to the same object
+        }
+        friend
+        bool operator!=(const _iterator& a, const _iterator& b) noexcept {return !(a == b);}
+    };
+    // ####################### END ITERATOR CLASS DEF
+
+    // the bst needs a node and a ptr to the head
     node _node;
     std::unique_ptr<node> head;
 
+
+    using iterator = _iterator<t_k>;
+    using const_iterator = _iterator<const t_k>;
+    iterator begin() noexcept {return iterator{left_most()};}
+    const_iterator begin() const noexcept {return const_iterator{left_most()};}
+    const_iterator cbegin() const noexcept {return const_iterator{left_most()};}
+
+    iterator end() noexcept {return iterator{nullptr};}
+    const_iterator end() const noexcept{return const_iterator{nullptr};}
+    const_iterator cend() const noexcept{return const_iterator{nullptr};}
+
+    // auxiliary function to retrieve the node with minimum (wrt OP) key
+    node* left_most() {
+        auto tmp = head.get();
+        while(tmp){
+            tmp = tmp->left;
+        }
+        return tmp;
+    }
+
     template <typename O>
-    class iterator; // defined in iterator_bst.cpp
+    std::pair<iterator, bool> _insert(O&& x){
+        bool inserted{false};
+        // if the node is already present we return <nullptr, false>
+        if (find(std::forward<O>(x.first))) {return std::pair<iterator, bool>{nullptr,inserted};} 
+        
+        // we traverse the bst starting from the head
+        auto tmp{head.get()};
+        while(inserted == false) // at this point we are sure that a new node wll be inserted so we stop when we succeed
+        {
+            // at every iteration of the while cicle we 
+            // visit a level of the bst until we reach the leaves
+
+            if ( OP(std::forward<O>(x.first),tmp->_pair.first) ){      // key_to_be_inserted < key_in_bst
+                if(tmp->left == nullptr){                              // if we are at a leaf (wrt insertion)
+                    tmp->left = new node{std::forward<O>(x.first)};    // append a new node
+                    inserted = true;                                   // notify we are done
+                    return std::pair<iterator, bool>{tmp->left,inserted};   // return
+                }
+                tmp = tmp->left;                                       // otherwise we just move to the left
+            }
+            else{                                                      // key_to_be_inserted > key_in_bst
+                if(tmp->right == nullptr){                             // if we are at a leaf (wrt insertion)
+                    tmp->right = new node{std::forward<O>(x.first)};   // append a new node
+                    inserted = true;                                   // notify we are done
+                    return std::pair<iterator, bool>{tmp->right,inserted};  // return
+                }
+                tmp = tmp->right;
+            }
+        }   
+    }
+
+public:
 
     // custom ctors and destructor
     bst() noexcept = default;
@@ -48,39 +161,84 @@ class bst{
 
     // mv sem:
     // mv ctor
-    bst(bst&& x) noexcept: _node{std::move(x._node)}, head{std::move(x.head)}
+    bst(bst&& x) noexcept: _node{std::move(x._node)}, head{std::move(x.head)} {}
     // mv assignment
     bst& operator==(bst&& x) noexcept{
         head = std::move(x.head);
         _node = std::move(x._node);
         return *this;
     }
+    
+    // xp sem:
+    // cp ctor
+    bst(const bst& x){
+        if(x.head){                     
+            head.reset(new node{x.head});   // as fare as x is not an empty bst I copy it by
+        }                                   // calling recursively the node ctor
+    }
+    // cp assignment
+    bst& operator=(const bst& x){
+        auto tmp{x};                 // construct a copy of x
+        *(this) = std::move(tmp);    // move it into myself
+        return *this;                // return myself
+    }
 
-    // here I need cp sem.
+    void clear() {head.reset();}
 
-    iterator<t_k> begin() noexcept {
-        // to traverse the bst I must start from the most left
-        // node. I can find it by starting from the head
-        // and going one on the left side until I meet a nullptr
-        // that will be the left most node and the point from which
-        // I have to start
-        auto tmp = iterator{head.get()};   
-        while(tmp.current != nullptr){
-            tmp.current = (tmp.current)->left;
+    iterator find(const t_k& x){
+        auto tmp{head.get()};
+        // we traverse the bst untill tmp is nullptr
+        while (tmp)     
+        {
+            if(tmp->_pair.first == x){  // found it
+                return iterator{tmp};
+            }
+            // otherwise move to the right or left child
+            if(tmp->_pair.first < x){
+                tmp = tmp->right;
+            }
+            else{ tmp = tmp->left;}
         }
-        return tmp;    
+        // if the while loop did not return means that x is not in the bst
+        return iterator{nullptr};
+        
     }
-
-    iterator<const t_k> begin() const noexcept {
-        return static_cast<iterator<const t_k>(begin());   
+    // same strategy as above but the returned iterator is constructed as a const one
+    const_iterator find(const t_k& x) const {
+        auto tmp{head.get()};
+        // we traverse the bst untill tmp is nullptr
+        while (tmp)     
+        {
+            if(tmp->_pair.first == x){  // found it
+                return const_iterator{tmp};
+            }
+            // otherwise move to the right or left child
+            if(tmp->_pair.first < x){
+                tmp = tmp->right;
+            }
+            else{ tmp = tmp->left;}
+        }
+        // if the while loop did not return means that x is not in the bst
+        return const_iterator{nullptr};
     }
-
-
 
     
+    // both the versions of insert
+    // call the private membe function _insert 
+    // that exploits forwarding refernces
+    // such that we wrote one function for two purposes
+    std::pair<iterator, bool> insert(const std::pair<const t_k,t_v>& x) {return _insert(x);}
+    std::pair<iterator, bool> insert(std::pair<const t_k,t_v>&& x)      {return _insert(x);}
 
-
-
+    // put to operator
+    friend
+    std::ostream& operator<<(std::ostream& os, const bst& x){
+        for(const auto& key : x){
+            os << key;
+        }
+        os << std::endl;
+        return os;
+    }
 };
 
 
